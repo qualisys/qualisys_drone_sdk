3705db7e7f95cddc40f2d0108c671334de86a5a2
mbaytas <m@baytas.net>
2022-04-27 22:22:06 +0200

    Add class for parallel context managers

--- qfly/utils.py
+++ qfly/utils.py
@@ -1,4 +1,7 @@
 import math
+import sys
+import threading
+import traceback
 
 
 def sqrt(x):
@@ -17,3 +20,75 @@ def pol2cart(rho, phi):
     x = rho * math.cos(math.radians(phi))
     y = rho * math.sin(math.radians(phi))
     return(x, y)
+
+
+class ParallelContextManagers(object):
+    """
+    Concurrently start and stop multiple context managers in different threads.
+
+    Usage:
+
+    with ParallelContextManagers(Foo(), Bar()) as managers:
+        foo, bar = managers
+        foo.do_something()
+        bar.do_something()
+    """
+
+    def __init__(self, *managers):
+        self.managers = managers
+
+    def __enter__(self):
+        errors = []
+        threads = []
+
+        for mgr in self.managers:
+            t = threading.Thread(target=self.run,
+                                 args=(mgr.__enter__, tuple(), errors))
+            t.start()
+            threads.append(t)
+        for thread in threads:
+            thread.join()
+
+        if errors:
+            err = MultipleError(errors)
+            raise err
+
+        return self.managers
+
+    def __exit__(self, *exc_info):
+        errors = []
+        threads = []
+
+        for mgr in self.managers:
+            t = threading.Thread(target=self.run,
+                                 args=(mgr.__exit__, exc_info, errors))
+            t.start()
+            threads.append(t)
+
+        for thread in threads:
+            thread.join()
+
+        if errors:
+            raise MultipleError(errors)
+
+    def run(func, args, errors):
+        try:
+            func(*args)
+        except:
+            errors.append(sys.exc_info())
+
+
+class MultipleError(Exception):
+    """
+    Exception class to collect several errors in a single object.
+    """
+
+    def __init__(self, errors):
+        super(Exception, self).__init__()
+        self.errors = errors
+
+    def __str__(self):
+        bits = []
+        for exc_type, exc_val, exc_tb in self.errors:
+            bits.extend(traceback.format_exception(exc_type, exc_val, exc_tb))
+        return "".join(bits)
